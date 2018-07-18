a3ec5b6cd67fb1942cb357ffdd766f048c17769a
Eric <Broadbent52@aol.com>
2018-07-18 08:41:44 -0600

    set up auth routs and header comp

---
+++ src/components/auth/signin.js
@@ -0,0 +1,13 @@
+import React, {Component} from 'react';
+
+class Signin extends Component{
+    render() {
+        return (
+            <div ClassName='sign-in'>
+                ...signin
+            </div>
+        )
+    }
+}
+
+export default Signin;
---
+++ src/components/auth/signup.js
@@ -0,0 +1,13 @@
+import React, {Component} from 'react';
+
+class Signup extends Component{
+    render() {
+        return (
+            <div ClassName='sign-up'>
+                ...signup
+            </div>
+        )
+    }
+}
+
+export default Signup;
