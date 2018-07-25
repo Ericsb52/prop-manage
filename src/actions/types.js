a66ff246dc193e760775e13d263b53546593c9c2
Eric <Broadbent52@aol.com>
2018-07-25 12:04:09 -0600

    start of setup of newsletter fetch

--- src/actions/types.js
+++ src/actions/types.js
@@ -1,1 +1,4 @@
-export const AUTHENTICATE_USER = 'AUTHENTICATE_USER'
+export const AUTHENTICATE_USER = 'AUTHENTICATE_USER';
+
+//newslettet actions
+export const SET_NEWSLETTERS = 'SET_NEWSLETTERS';
--- src/components/newsletter/newsletterGrid.js
+++ src/components/newsletter/newsletterGrid.js
@@ -1,4 +1,6 @@
 import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import * as actions from '../../actions'
 
 import NewsletterBox from './newsletterBox';
 import NewsletterArchive from './newsletterArchive';
@@ -11,15 +13,14 @@ class NewsletterGrid extends Component {
         this.props.history.push('/newsletter/new');
     }
 
+    componentDidMount(){
+        this.props.fetchNewsletters();
+    }
+
     render() {
 
         const latest = {
-            _id: '115',
-            title: 'Happy Holidays Fam',
-            body: 'is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry\'s standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum',
-            date: new Date(),
-            imageUrl: 'http://via.placeholder.com/960x258'
-        }
+              }
 
         return (
             <div className='newsletter-grid'>
@@ -32,4 +33,4 @@ class NewsletterGrid extends Component {
     }
 }
 
-export default NewsletterGrid;
+export default connect( null, actions )(NewsletterGrid);
